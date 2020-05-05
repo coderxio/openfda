b71cb08367d03d73bcc2f1404046b202975280a8
Hassan Alwan <34286501+hl1@users.noreply.github.com>
2020-05-04 22:36:30 -0500

    Update run.py

--- run.py
+++ run.py
@@ -3,20 +3,22 @@ import sys
 from load_ndcs import main as load_ndcs
 from connect_db import main as connect_db
 from server import main as cherrypy_server
+from helpers import startLogging
 
-logger = startLogging('error')
+logger = startLogging('run')
+
 
 def main():
     # Allow the DB to set up completely.
     time.sleep(15)
     try:
         # Grab drug classes
-        sys.stdout.write("Connecting to database\n")
+        logger.debug("Connecting to database\n")
         session = connect_db()
     except:
-        sys.stderr.write("Failed connecting to database\n")
+        logger.warning("Failed connecting to database\n")
         time.sleep(60)
-        sys.stderr.write("Second attempt connecting to database\n")
+        logger.error("Second attempt connecting to database\n")
         session = connect_db()
 
     cherrypy_server()
