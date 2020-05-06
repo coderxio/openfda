6fe2e5bd9eab585704d4bc5a3625e20d80035d7e
Caleb Dunn <finish.06@gmail.com>
2020-05-06 07:44:40 -0500

    add routes table & pharm_class table

--- connect_db.py
+++ connect_db.py
@@ -1,25 +1,49 @@
 import requests
 import sys
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 
 # Table schema
 Base = declarative_base()
+
+# Main table
 class Drugs(Base):
-    __tablename__ = "drug_info"
+    __tablename__ = "drug"
 
     id = Column(Integer, primary_key=True)
+    product_id = Column(String(100))
+    
     generic_name = Column(String(300))
     brand_name = Column(String(300))
     pharm_class = Column(String(100))
     dosage_form = Column(String(100))
-    route = Column(String(100))
+    routes = relationship("Routes", backref='dx_route')
 
     def __repr__(self):
-        return "<Drugs(generic_name='%s', pharm_class='%s')>" % (
-                    self.generic_name, self.pharm_class)
+        return "<Drugs(generic_name='%s', brand_name='%s', pharm_class='%s')>" % (
+                    self.generic_name, self.brand_name, self.pharm_class)
+
+
+# Routes table
+class Routes(Base):
+    __tablename__ = "route"
+
+    id = Column(Integer, primary_key=True)
+    route = Column(String(100))
+    drug_id = Column(Integer, ForeignKey('drug.id'))
+
+    def __repr__(self):
+        return "<Routes(route='%s')>" % (self.route)
+    
+
+# Pharmacology Class table
+class PharmClass(Base):
+    __tablename__ = "pharmClass"
+
+    id = Column(Integer, primary_key=True)
+    product_type = Column(String(30))
 
 def main():
     # Connect to DB
