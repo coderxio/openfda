860bfef9c674aee0a022d7878f7be5ce5cbc65ab
Caleb Dunn <finish.06@gmail.com>
2020-05-07 20:56:33 -0500

    Remove logic for connecting to DB

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import time
 import sys
 from load_ndcs import main as load_ndcs
 from connect_db import main as connect_db
@@ -10,17 +9,8 @@ logger = startLogging('run')
 
 def main():
     # Allow the DB to set up completely.
-    time.sleep(15)
-    try:
-        # Grab drug classes
-        logger.debug("Connecting to database\n")
-        session = connect_db()
-    except:
-        logger.warning("Failed connecting to database\n")
-        time.sleep(60)
-        logger.error("Second attempt connecting to database\n")
-        session = connect_db()
-
+    session = connect_db()
+    
     # cherrypy_server()
     load_ndcs(session)
 
