534bc1329fe34ff77eb91255cf62727c301629c1
Caleb Dunn <finish.06@gmail.com>
2020-05-12 11:30:25 -0500

    add brand/generic subdir

--- server.py
+++ server.py
@@ -62,19 +62,21 @@ class SATool(cherrypy.Tool):
 
 class Root():
     def _cp_dispatch(self, vpath):
-        if len(vpath) == 1:
+        if len(vpath) == 2:
+            cherrypy.request.params['root'] = vpath.pop()
             cherrypy.request.params['genericName'] = vpath.pop()
             return self
 
     @cherrypy.expose
-    def index(self, genericName):
-        rows = set([str(row[0]) for row in DrugsList.findBrandName(cherrypy.request.db, genericName)])
-        output = []
-        for item in rows:
-            match = re.search(rf'\b{genericName}\b', item)
-            if match is None:
-                output.append(item)
-        return f"{genericName}: {', '.join(output)}"
+    def index(self, root, genericName):
+        if root.lower() == 'generic':
+            rows = set([str(row[0]) for row in DrugsList.findBrandName(cherrypy.request.db, genericName)])
+            output = []
+            for item in rows:
+                match = re.search(rf'\b{genericName}\b', item)
+                if match is None:
+                    output.append(item)
+            return f"{genericName}: {', '.join(output)}"
 
 
 def main():
