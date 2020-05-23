e4878be6c46540e71b4779c4d30ea659378d0457
Caleb Dunn <finish.06@gmail.com>
2020-05-23 14:31:23 -0500

    fix sql link

--- app/load_data/connect_db.py
+++ app/load_data/connect_db.py
@@ -1,16 +1,21 @@
 import os
 import requests
 import sys
+from pathlib import Path
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from load_data.models import Base
+from helpers.helpers import startLogging
 
+logger = startLogging('connect_db')
 
 def main():
     # Connect to DB
-    sys.stderr.write("Connecting to the DB")
-    engine = create_engine(os.environ.get('DB_URI', '../../sqlite:///drugs.db'))
+    p = Path.cwd()
+    db = f"sqlite:///{p.parent / 'data'}/drugs.db"
+    logger.debug(db)
+    engine = create_engine(os.environ.get('DB_URI', db))
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
