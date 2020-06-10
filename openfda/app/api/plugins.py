80ee35fcdff8754b80d977707f00f415b204b89a
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2020-06-09 18:58:00 -0700

    Have cherrypy create db tables on init

--- openfda/app/api/plugins.py
+++ openfda/app/api/plugins.py
@@ -1,8 +1,12 @@
 import os
+import time
+import logging
 from pathlib import Path
 from cherrypy.process import wspbus, plugins
 from sqlalchemy import create_engine
 from db.models import Base
+
+logger = logging.getLogger('run')
 
 class SAEnginePlugin(plugins.SimplePlugin):
     def __init__(self, bus):
@@ -14,6 +18,13 @@ class SAEnginePlugin(plugins.SimplePlugin):
         p = Path.cwd()
         db = f"sqlite:///drugs.db"
         self.sa_engine = create_engine(os.environ.get('DB_URI', db))
+        try:
+            Base.metadata.create_all(self.sa_engine)
+        except Exception as e:
+            logger.warning('Failed to connect to db')
+            time.sleep(30)
+            logger.error('Second attempt to connect')
+            Base.metadata.create_all(self.sa_engine)
 
     def stop(self):
         if self.sa_engine:
